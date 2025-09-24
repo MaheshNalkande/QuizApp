1c66aec02db19ce09305861fa851cb57b59d7730
MaheshNalkande <mdnalkande@gmail.com>
2025-09-24 18:56:43 +0530

    APIs for save, getByCategory, getAll

--- src/main/java/com/quizapp/controller/QuestionController.java
+++ src/main/java/com/quizapp/controller/QuestionController.java
@@ -1,14 +1,33 @@
 package com.quizapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.quizapp.entity.Question;
+import com.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/question")
 public class QuestionController {
+    @Autowired
+    QuestionService questionService;
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
-        return "These are all questions....";
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
     }
+    @GetMapping("/category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+       return questionService.getQuestionsByCategory(category);
+    }
+    @PostMapping("/addQuestions")
+    public void addQuestions(@RequestBody List<Question> questions){
+        questionService.addQuestions(questions);
+        System.out.println("Added Successfully");
+    }
+    @PostMapping("/addQuestion")
+    public Question addOneQuestion(@RequestBody Question question){
+         return questionService.addOneQuestion(question);
+    }
+
 }
---
+++ src/main/java/com/quizapp/service/QuestionService.java
@@ -0,0 +1,33 @@
+package com.quizapp.service;
+
+import com.quizapp.dao.QuestionDao;
+import com.quizapp.entity.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+//    Get all questions
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+
+    }
+//Find By Category
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+//    Save ALL
+    public void addQuestions(List<Question> questions){
+         questionDao.saveAll(questions);
+
+    }
+// Save one Questions
+    public Question addOneQuestion(Question question){
+        return  questionDao.save(question);
+
+    }
+}
