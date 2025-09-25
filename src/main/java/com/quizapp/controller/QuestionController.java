569265bd80275fbe2c40eee7e29a28ef202ab692
MaheshNalkande <mdnalkande@gmail.com>
2025-09-25 17:33:09 +0530

    Again modified

--- src/main/java/com/quizapp/controller/QuestionController.java
+++ src/main/java/com/quizapp/controller/QuestionController.java
@@ -3,6 +3,8 @@ package com.quizapp.controller;
 import com.quizapp.entity.Question;
 import com.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,21 +15,25 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
     @GetMapping("/category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
        return questionService.getQuestionsByCategory(category);
     }
     @PostMapping("/addQuestions")
-    public void addQuestions(@RequestBody List<Question> questions){
-        questionService.addQuestions(questions);
-        System.out.println("Added Successfully");
+    public ResponseEntity<String> addQuestions(@RequestBody List<Question> questions){
+        return questionService.addQuestions(questions);
+
     }
     @PostMapping("/addQuestion")
-    public Question addOneQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addOneQuestion(@RequestBody Question question){
          return questionService.addOneQuestion(question);
     }
 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Integer id){
+        return  questionService.deleteById(id);
+    }
 }
