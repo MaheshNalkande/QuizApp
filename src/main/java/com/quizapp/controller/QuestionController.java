782378b63d78c3deb6cff99cd550698086935a02
MaheshNalkande <mdnalkande@gmail.com>
2025-09-25 17:12:00 +0530

    Done with ResponseEnity and Exception Handling

--- src/main/java/com/quizapp/controller/QuestionController.java
+++ src/main/java/com/quizapp/controller/QuestionController.java
@@ -3,7 +3,6 @@ package com.quizapp.controller;
 import com.quizapp.entity.Question;
 import com.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,26 +12,22 @@ import java.util.List;
 public class QuestionController {
     @Autowired
     QuestionService questionService;
-
     @GetMapping("allQuestions")
-    public ResponseEntity<List<Question>> getAllQuestions() {
+    public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
     }
-
     @GetMapping("/category/{category}")
-    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
-        return questionService.getQuestionsByCategory(category);
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+       return questionService.getQuestionsByCategory(category);
     }
-
     @PostMapping("/addQuestions")
-    public ResponseEntity<String> addQuestions(@RequestBody List<Question> questions) {
-        return questionService.addQuestions(questions);
-
+    public void addQuestions(@RequestBody List<Question> questions){
+        questionService.addQuestions(questions);
+        System.out.println("Added Successfully");
     }
-
     @PostMapping("/addQuestion")
-    public ResponseEntity<String> addOneQuestion(@RequestBody Question question) {
-        return questionService.addOneQuestion(question);
+    public Question addOneQuestion(@RequestBody Question question){
+         return questionService.addOneQuestion(question);
     }
 
 }
--- src/main/java/com/quizapp/service/QuestionService.java
+++ src/main/java/com/quizapp/service/QuestionService.java
@@ -3,61 +3,31 @@ package com.quizapp.service;
 import com.quizapp.dao.QuestionDao;
 import com.quizapp.entity.Question;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class QuestionService {
     @Autowired
     QuestionDao questionDao;
-
-    //    Get all questions
-    public ResponseEntity<List<Question>> getAllQuestions() {
-        try {
-            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.FOUND);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-//        If nothing is found return empty arraylist
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
-    }
-
-    //Find By Category
-    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
-        try {
-            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.FOUND);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
-    }
-
-    //    Save ALL
-    public ResponseEntity<String> addQuestions(List<Question> questions) {
-        try {
-            questionDao.saveAll(questions);
-            return new ResponseEntity<>("Success", HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>("Not saved", HttpStatus.NOT_MODIFIED);
+//    Get all questions
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
 
     }
+//Find By Category
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+//    Save ALL
+    public void addQuestions(List<Question> questions){
+         questionDao.saveAll(questions);
 
-    // Save one Questions
-    public ResponseEntity<String> addOneQuestion(Question question) {
-        questionDao.save(question);
-        try {
-            questionDao.save(question);
-            return new ResponseEntity<>("Success", HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>("Not saved", HttpStatus.NOT_MODIFIED);
+    }
+// Save one Questions
+    public Question addOneQuestion(Question question){
+        return  questionDao.save(question);
 
     }
 }
